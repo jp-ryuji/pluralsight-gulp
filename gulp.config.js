0992da223eb946821bdf56a8851767e882d03969
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-09 21:04:23 +0900

    Add task for wiredep

--- gulp.config.js
+++ gulp.config.js
@@ -1,14 +1,41 @@
 module.exports = function() {
   var client = './src/client/';
+  var clientApp = client + 'app/';
+
   var config = {
     temp: './.tmp/',
 
+    // Files paths
     alljs: [
       './src/**/*.js',
       './*.js'
     ],
+    client: client,
+    index: client + 'index.html',
+    js: [
+      clientApp + '**/*.module.js', // The naming rule complies with Angular
+      clientApp + '**/*.js',
+      '!' + clientApp + '**/*.spec.js' // '!' is for excluding files
+    ],
 
-    less: client + 'styles/styles.less'
+    less: client + 'styles/styles.less',
+
+    // Bower and NPM locations
+    bower: {
+      json: require('./bower.json'),
+      directory: './bower_components/',
+      ignorePath: '../..'
+    }
+  };
+
+  config.getWiredepDefaultOptions = function() {
+    var options = {
+      bowerJson: config.bower.json,
+      directory: config.bower.directory,
+      ignorePath: config.bower.ignorePath
+    };
+
+    return options;
   };
 
   return config;
--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,19 @@ gulp.task('less-watcher', function() {
   gulp.watch([config.less], ['styles']);
 });
 
+gulp.task('wiredep', function() {
+  log('Wire up the bower css js and our app js into the html');
+
+  var options = config.getWiredepDefaultOptions();
+  var wiredep = require('wiredep').stream;
+
+  return gulp
+    .src(config.index)
+    .pipe(wiredep(options))
+    .pipe($.inject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
+
 function errorLogger(error) {
   log('*** Start of Error ***');
   log(error);
