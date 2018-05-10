eb0cc4c10361c532e4091f741baff210b14eb051
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-10 14:05:26 +0900

    Add task for templatecache

--- gulpfile.js
+++ gulpfile.js
@@ -69,17 +69,40 @@ gulp.task('clean-styles', function(done) {
   clean(config.temp + '**/*.css', done);
 });
 
-gulp.task('clean-code', function(done) {
+// gulp.task('clean-code', function(done) {
+//   var files = [].concat(
+//     config.temp + '**/*.js',
+//     config.build + '**/*.html',
+//     config.build + 'js/**/*.js',
+//   );
+//   clean(files, done);
+// });
+
+// FIXME: With done, templatecache doesn't work.
+gulp.task('clean-code', function() {
   var files = [].concat(
     config.temp + '**/*.js',
     config.build + '**/*.html',
     config.build + 'js/**/*.js',
   );
-  clean(files, done);
+  clean(files);
 });
 
 gulp.task('less-watcher', function() {
   gulp.watch([config.less], ['styles']);
+});
+
+gulp.task('templatecache', ['clean-code'], function() {
+  log('Creating AngularJS $templateCache');
+
+  return gulp
+    .src(config.htmltemplates)
+    .pipe($.minifyHtml({ empty: true }))
+    .pipe($.angularTemplatecache(
+      config.templateCache.file,
+      config.templateCache.options,
+      ))
+    .pipe(gulp.dest(config.temp));
 });
 
 gulp.task('wiredep', function() {
