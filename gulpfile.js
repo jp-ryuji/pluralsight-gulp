260f14482d706bfc728c08d248dab5322fbddd7f
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-12 11:57:41 +0900

    Add task for bump

--- gulpfile.js
+++ gulpfile.js
@@ -152,6 +152,35 @@ gulp.task('optimize', ['inject', 'fonts', 'images'], function() {
 
 });
 
+// Bump the version
+// * --type=pre will bump the prerelease version *.*.*-x
+// * --type=patch or no flag will bump the patch verison *.*.x
+// * --type=minor will bump the minor version *.x.*
+// * --type=major will bump the major version x.*.*
+// * --version=1.2.3 will bump to a specific version and ignore other flags
+gulp.task('bump', function() {
+  var msg = 'Bumping versions';
+  var type = args.type;
+  var version = args.version;
+  var options = {};
+
+  if (version) {
+    options.version = version;
+    msg += ' to ' + version;
+  } else {
+    options.type = type;
+    msg += ' for a ' + type;
+  }
+
+  log(msg);
+
+  return gulp
+    .src(config.packages)
+    .pipe(print())
+    .pipe($.bump(options))
+    .pipe(gulp.dest(config.root));
+});
+
 // FIXME: The files are concatinated and minified correctly, but separated files seem to be sent to the browser.
 gulp.task('serve-build', ['optimize'], function() {
   serve(false);
